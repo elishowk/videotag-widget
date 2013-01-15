3989a9d27a6d68a49230b6d789d77ef91c9e2645
Avétis KAZARIAN <avetis.k@gmail.com>
2013-01-15 19:34:44 +0100

    upgrade makefile

--- static/videotag-widget/main.js
+++ static/videotag-widget/main.js
@@ -1,40 +1,40 @@
 /*global define, _*/
 
 require.config({
-  'baseUrl': require.appConfig.baseUrl,
-  'paths': {
-    'domready':   'lib/require-domready-2.0.1.min',
-    'text':       'lib/require-text-2.0.3.min',
-    'i18n':       'lib/require-i18n-2.0.1.min',
-    'noext':      'lib/require-noext-0.3.1.min',
-    'backbone':   'lib/backbone-0.9.9.min',
-    'jquery':     'lib/jquery-1.8.3.min',
-    'underscore': 'lib/lodash-1.0.0.min',
-    'app':        'videotag-widget/app',
-    'modules':    'videotag-widget/modules',
-  },
-  'shim': {
-    'backbone': {
-      'deps': [
-        'underscore',
-        'jquery'
-      ],
-      'exports': 'Backbone'
+    'baseUrl': require.appConfig.baseUrl,
+    'paths': {
+        'domready':     'lib/require-domready-2.0.1.min',
+        'text':             'lib/require-text-2.0.3.min',
+        'i18n':             'lib/require-i18n-2.0.1.min',
+        'noext':            'lib/require-noext-0.3.1.min',
+        'backbone':     'lib/backbone-0.9.9.min',
+        'jquery':         'lib/jquery-1.8.3.min',
+        'underscore': 'lib/lodash-1.0.0.min',
+        'app':                'videotag-widget/app',
+        'modules':        'videotag-widget/modules',
     },
-    'youtube-player-api': {
-      'exports': 'YT'
-    },
-  }
+    'shim': {
+        'backbone': {
+            'deps': [
+                'underscore',
+                'jquery'
+            ],
+            'exports': 'Backbone'
+        },
+        'youtube-player-api': {
+            'exports': 'YT'
+        },
+    }
 });
 
 define([
-  'app',
-  'poser/backbone-tastypie-0.1',
+    'app',
+    'poser/backbone-tastypie-0.1',
 ], function (App) {
-  'use strict';
+    'use strict';
 
-  App.on('ready', function () {
-    $(document.body).append(App.view.$el);
-  });
-  App.initialize();
+    App.on('ready', function () {
+        $(document.body).append(App.view.$el);
+    });
+    App.initialize();
 });
