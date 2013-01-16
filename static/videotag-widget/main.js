042be2e623c829245cdc20b5a317a73d134fce0f
Avétis KAZARIAN <avetis.k@gmail.com>
2013-01-16 09:45:57 +0100

    fix main.js

--- static/videotag-widget/main.js
+++ static/videotag-widget/main.js
@@ -4,14 +4,13 @@ require.config({
     'baseUrl': require.appConfig.baseUrl,
     'paths': {
         'domready':     'lib/require-domready-2.0.1.min',
-        'text':             'lib/require-text-2.0.3.min',
-        'i18n':             'lib/require-i18n-2.0.1.min',
-        'noext':            'lib/require-noext-0.3.1.min',
+        'i18n':         'lib/require-i18n-2.0.1.min',
+        'noext':        'lib/require-noext-0.3.1.min',
         'backbone':     'lib/backbone-0.9.9.min',
-        'jquery':         'lib/jquery-1.8.3.min',
-        'underscore': 'lib/lodash-1.0.0.min',
-        'app':                'videotag-widget/app',
-        'modules':        'videotag-widget/modules',
+        'jquery':       'lib/jquery-1.8.3.min',
+        'underscore':   'lib/lodash-1.0.0.min',
+        'app':          'videotag-widget/app',
+        'modules':      'videotag-widget/modules',
     },
     'shim': {
         'backbone': {
